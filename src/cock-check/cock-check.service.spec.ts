84f71ab0a7a1c9dc348eef3f11915e5129434463
Aleksandr Bukhalo <a@bukhalo.com>
2020-09-09 11:27:30 +0300

    fix: tests

--- src/cock-check/cock-check.service.spec.ts
+++ src/cock-check/cock-check.service.spec.ts
@@ -1,11 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CockCheckService } from './cock-check.service';
+import { ConfigModule } from '@nestjs/config';
+import { BullModule } from '@nestjs/bull';
 
 describe('CockCheckService', () => {
   let service: CockCheckService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule,
+        BullModule.registerQueue({
+          name: 'cockCheck',
+        }),
+      ],
       providers: [CockCheckService],
     }).compile();
 
--- src/cock-check/cock-check.update.spec.ts
+++ src/cock-check/cock-check.update.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { ConfigModule } from '@nestjs/config';
+import { BullModule } from '@nestjs/bull';
 import { CockCheckUpdate } from './cock-check.update';
+import { CockCheckService } from './cock-check.service';
 
 describe('CockCheckService', () => {
   let service: CockCheckUpdate;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CockCheckUpdate],
+      imports: [
+        ConfigModule,
+        BullModule.registerQueue({
+          name: 'cockCheck',
+        }),
+      ],
+      providers: [CockCheckUpdate, CockCheckService],
     }).compile();
 
     service = module.get<CockCheckUpdate>(CockCheckUpdate);
