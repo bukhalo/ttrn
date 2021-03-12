c30f440b40f0849bafe26c905bdba4c045fb234c
Alexander Bukhalo <a@bukhalo.com>
2021-03-12 16:07:20 +0300

    Restrict meme handlers for supergroups only

--- main.py
+++ main.py
@@ -27,28 +27,40 @@ async def mention_all(msg: types.Message):
                      '@r_levkovych, @sunnydaily, @kirich_l, @Derik117')
 
 
-@dp.message_handler(filters.Text(equals=['дока', 'доку', 'документация', 'документацию'], ignore_case=True))
+@dp.message_handler(
+    filters.Text(equals=['дока', 'доку', 'документация', 'документацию'], ignore_case=True),
+    chat_type=types.ChatType.SUPERGROUP
+)
 async def doka(msg: types.Message):
     await msg.answer('Доку профессора писали наверное')
 
 
-@dp.message_handler(filters.Text(equals=['пидор', 'пидорас'], ignore_case=True))
+@dp.message_handler(
+    filters.Text(equals=['пидор', 'пидорас'], ignore_case=True),
+    chat_type=types.ChatType.SUPERGROUP
+)
 async def pidor(msg: types.Message):
     await msg.answer('пидор')
 
 
-@dp.message_handler(filters.Text(equals=['тотарен', 'тотарин', 'толик', 'толян', 'еболик'], ignore_case=True))
+@dp.message_handler(
+    filters.Text(equals=['тотарен', 'тотарин', 'толик', 'толян', 'еболик'], ignore_case=True),
+    chat_type=types.ChatType.SUPERGROUP
+)
 async def totaren(msg: types.Message):
     await msg.answer('пидор')
 
 
-@dp.message_handler(filters.Text(equals=['ярик', 'ярек', 'ярослав'], ignore_case=True))
+@dp.message_handler(
+    filters.Text(equals=['ярик', 'ярек', 'ярослав'], ignore_case=True),
+    chat_type=types.ChatType.SUPERGROUP
+)
 async def yarek(msg: types.Message):
     if decision(0.5):
         await msg.answer('Вы всё ещё готовите на огне @yaroslav_y? Тогда мы идём к вам.')
 
 
-@dp.message_handler()
+@dp.message_handler(chat_type=types.ChatType.SUPERGROUP)
 async def toopa(msg: types.Message):
     if decision(0.05):
         await msg.reply('ТУПА АЛЕГАНТОР))))))')
