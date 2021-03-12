897008de8dcaa288a92d3010d209a6bd7db67940
Alexander Bukhalo <a@bukhalo.com>
2021-03-12 16:04:57 +0300

    Support reposting for `text`, `sticker` and `dice` message types.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from os import getenv
 from aiogram import Bot, Dispatcher, types, executor, filters
 
 TOKEN = getenv('TOKEN')
+GROUP_ID = getenv('GROUP')
 logging.basicConfig(level=logging.DEBUG)
 
 bot = Bot(token=TOKEN)
@@ -51,6 +52,49 @@ async def yarek(msg: types.Message):
 async def toopa(msg: types.Message):
     if decision(0.05):
         await msg.reply('ТУПА АЛЕГАНТОР))))))')
+
+
+@dp.message_handler(
+    content_types=[
+        types.ContentType.TEXT,
+        types.ContentType.DICE,
+        types.ContentType.STICKER
+    ],
+    chat_type=types.ChatType.PRIVATE
+)
+async def repost(msg: types.Message):
+    content_type = msg.content_type
+    if content_type is types.ContentType.TEXT:
+        await bot.send_message(chat_id=GROUP_ID, text=msg.text)
+    # if self.audio:
+    #     return ContentType.AUDIO
+    # if self.animation:
+    #     return ContentType.ANIMATION
+    # if self.document:
+    #     return ContentType.DOCUMENT
+    # if self.game:
+    #     return ContentType.GAME
+    # if self.photo:
+    #     return ContentType.PHOTO
+    if content_type is types.ContentType.STICKER:
+        await bot.send_sticker(chat_id=GROUP_ID, sticker=msg.sticker.file_id)
+    # if self.video:
+    #     return ContentType.VIDEO
+    # if self.video_note:
+    #     return ContentType.VIDEO_NOTE
+    # if self.voice:
+    #     return ContentType.VOICE
+    # if self.contact:
+    #     return ContentType.CONTACT
+    # if self.venue:
+    #     return ContentType.VENUE
+    # if self.location:
+    #     return ContentType.LOCATION
+    # if self.poll:
+    #     return ContentType.POLL
+    if content_type is types.ContentType.DICE:
+        await bot.send_dice(chat_id=GROUP_ID)
+
 
 if __name__ == '__main__':
     executor.start_polling(dp)
