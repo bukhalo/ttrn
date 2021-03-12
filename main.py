4883df95e7829c7eea14315b3afcad6ca5265940
Alexander Bukhalo <a@bukhalo.com>
2021-03-12 18:49:00 +0300

    Shield functional added. Simple robot checker for new users.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import random
 from os import getenv
 from typing import List
 from aiogram import Bot, Dispatcher, types, executor, filters
+from aiogram.utils.callback_data import CallbackData
 
 TOKEN = getenv('TOKEN')
 GROUP_ID = getenv('GROUP')
@@ -112,6 +113,44 @@ async def repost(msg: types.Message):
     if content_type is types.ContentType.DICE:
         await bot.send_dice(chat_id=GROUP_ID)
 
+
+shield = CallbackData("shield", "user_id", "action")
+
+
+@dp.message_handler(content_types=[types.ContentType.NEW_CHAT_MEMBERS])
+async def new_member(msg: types.Message):
+    if msg.from_user in msg.new_chat_members:
+        buttons = [
+            types.InlineKeyboardButton(text="Ні", callback_data=shield.new(user_id=msg.from_user.id, action="allow")),
+            types.InlineKeyboardButton(text="Підор", callback_data=shield.new(user_id=msg.from_user.id, action="block")),
+        ]
+        keyboard = types.InlineKeyboardMarkup(row_width=1)
+        keyboard.add(*buttons)
+        await msg.answer("Ти пидор чи ні?", reply_markup=keyboard)
+        await bot.restrict_chat_member(chat_id=msg.chat.id, user_id=msg.from_user.id)
+
+
+@dp.callback_query_handler(shield.filter(action=["allow"]))
+async def shield_allow(call: types.CallbackQuery, callback_data: dict):
+    user_id = int(callback_data['user_id'])
+    if call.from_user.id == user_id:
+        await bot.restrict_chat_member(
+            chat_id=call.message.chat.id,
+            user_id=call.from_user.id,
+            permissions=types.ChatPermissions(can_send_messages=True))
+        await call.answer('Прийнято!')
+    else:
+        await call.answer('Не для тебе кнопка зроблена, пес.')
+
+
+@dp.callback_query_handler(shield.filter(action=["block"]))
+async def shield_block(call: types.CallbackQuery, callback_data: dict):
+    user_id = int(callback_data['user_id'])
+    if call.from_user.id == user_id:
+        await call.answer('Прийнято!')
+        await bot.kick_chat_member(chat_id=call.message.chat.id, user_id=user_id)
+    else:
+        await call.answer('Не для тебе кнопка зроблена, пес.')
 
 if __name__ == '__main__':
     executor.start_polling(dp)
